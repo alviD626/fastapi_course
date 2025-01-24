f4f17bd45440b5d8de37824274e4ef6fc6cde454
Ali Mostakim Alvi <alvi@nitex.com>
2025-01-24 17:59:38 +0600

    add new topic

--- project2/books2.py
+++ project2/books2.py
@@ -23,12 +23,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id : Optional[int] = None
+    id : Optional[int] = Field(description = "ID is not needed on create", default=None )
     title : str = Field(min_length=3)
     author : str = Field(min_length=1)
     description : str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=6)
-
+    
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author" : "coding withroby",
+                "description" : "A new description of a book",
+                "rating" : 5
+            }
+        }
+    }
 
 
 BOOKS = [
@@ -48,6 +58,23 @@ async def read_all_book():
 
 
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
@@ -62,3 +89,19 @@ def find_book_id(book: Book):
     #     book.id = 1
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+
+
+@app.put("/books/update_book")
+async def update_book(book:BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
